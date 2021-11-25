5a521bbfb467e78dbd93d40e2e78051d7ccf507f
JayedAkbarSumon <akbarjayed87@gmail.com>
2021-11-25 15:34:09 +0600

    setup project

---
+++ src/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwkOTg8zJWPWvwHB4cDcTSx8jEE-8VYS8",
+    authDomain: "dazko-fitness.firebaseapp.com",
+    projectId: "dazko-fitness",
+    storageBucket: "dazko-fitness.appspot.com",
+    messagingSenderId: "779257485247",
+    appId: "1:779257485247:web:87ac68a1ece94ea523f30a"
+};
+export default firebaseConfig;
